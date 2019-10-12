c9a1813a75bac53b1d7dea9031abc49f84e7289d
Randy <ztelur@gmail.com>
2019-10-12 16:20:53 +0800

    update

---
+++ src/main/java/com/remcarpediem/demo/persistent/PersistentApplication.java
@@ -0,0 +1,13 @@
+package com.remcarpediem.demo.persistent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistentApplication.class, args);
+	}
+
+}
